e89e4205e4c2e380831053a3bae482c081f6322c
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-12-05 16:41:16 +0545

    -added repository

--- src/main/java/com/file/upload/repository/FileRepository.java
+++ src/main/java/com/file/upload/repository/FileRepository.java
@@ -1,9 +1,15 @@
 package com.file.upload.repository;
 
+import com.file.upload.model.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /*
  * @created 05/12/2020-4:37 PM
  * @project file
  * @author Sagar Siwakoti
  */
-public class FileRepository {
+@Repository
+public interface FileRepository extends JpaRepository<File, String> {
+
 }
